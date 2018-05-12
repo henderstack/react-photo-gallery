916b9fbc1ecec293b3fd6710f6d04b42fcd97380
Bryan Henderson <bhenderson71@gmail.com>
2018-05-11 22:22:12 -0500

    First Stage - initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Photo Gallery Module
+
+# This is the first stage of a Gallery Manager Project:
+
+- Stage 1: Photo Filter Component
+- Stage 2: Drag 'n Drop single/multi image loader
+- Stage 3: GraphQL API/AWS DynamoDB backend
+- Stage 4 (onward): I haven't thought that far ahead ;)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import PhotoEditFilter from './components/PhotoEditFilter';
+
+class App extends Component {
+  render() {
+    return (
+      <PhotoEditFilter />
+    );
+  }
+}
+
+export default App;
